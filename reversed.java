a02f729eeb3e000cc4576f54b818e1a716e4d0bc
Sudipya <sudipyasrivastava@gmail.com>
2025-04-14 14:22:48 +0530

    g

--- reversed.java
+++ reversed.java
@@ -1,16 +1,17 @@
 import java.util.*;
-public class Reversed{
+public class reversed{
 public static void main(String[]args){
 System.out.println("enter the number of which you want reverse");
-Scanner s=new Scanner(System.in);
-int num =s.nextInt();
-int sum ;
-int rev=0;
-while(num>0){
-sum=num%10;
-rev=rev*10+sum;
-num=num/10;
+try (Scanner s = new Scanner(System.in)) {
+    int num =s.nextInt();
+    int sum ;
+    int rev=0;
+    while(num>0){
+    sum=num%10;
+    rev=rev*10+sum;
+    num=num/10;
+    }
+    System.out.println("reversed number:"+rev);
 }
-System.out.println("reversed number:"+rev);
 }
 }
