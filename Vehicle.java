fc5ee99ba9da17271b3037eca96734686c5d4b9b
Sudipya <sudipyasrivastava@gmail.com>
2025-04-24 10:38:24 +0530

    Create Vehicle.java

---
+++ Vehicle.java
@@ -0,0 +1,18 @@
+interface Vehicle{
+    void brand();
+    void speed();
+    void fueltype();
+}
+class Car implements Vehicle{
+    @Override
+    public void brand{
+        System.out.println("your car brand is mercedeces");
+    }
+    public void speed{
+        System.out.println("your car speed is 200");
+    }
+    public void fueltype{
+        System.out.println("your car is compatible with petrol");
+    }
+    void seatingCapacity
+}
